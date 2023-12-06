0ad4c15f37a215109d92fab85f5941cff4c4d2cf
Farah Arar <farah02arar@gmail.com>
2023-12-06 14:33:07 +0300

    footer done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
-import Footer from "./componets/Footer"
-import Navbar from "./componets/Navbar"
+
 
 export default function Home() {
   return (
     <main>
-      <Navbar  />
-      <Footer />
+     
 
 
     </main>
